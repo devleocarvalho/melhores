c3bb9808351059acfc734c5c0092da8171816296
devleocarvalho <dev.leocarvalho@gmail.com>
2026-03-12 12:51:28 -0300

    foto

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,8 +1,8 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  // Substitua pela sua URL da Vercel ou seu domínio próprio
-  const baseUrl = 'https://www.melhoresdoano.com.br' // URL temporária, ajustar para o domínio final
+  
+  const baseUrl = 'https://melhoresdoano-wine.vercel.app/' // URL temporária, ajustar para o domínio final
 
   return [
     {
@@ -11,6 +11,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'daily',
       priority: 1,
     },
-    // Se você criar páginas individuais para cada cafeteira no futuro, elas entram aqui
+    
   ]
 }
